4775a1faf43cebb1f747bd63f980b9cc8d34dee5
habibmirsyad <habmirs@gmail.com>
2022-03-12 13:09:07 +0700

    menambahkan class kode buah

---
+++ KodeBuah.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KPL_1302201138_MOD3_JURNAL_GGR
+{
+    public class KodeBuah
+    {
+        public enum Buah
+        {
+            Apel, Alprikot, Alpukat, Pisang, Paprika, Blackberry,
+            Ceri, Kelapa, Jagung, Kurma, Durian, Anggur, Melon, Semangka
+        }
+
+        public static string getKodeBuah(Buah buah)
+        {
+            string[] kodeBuah = { "A00", "B00", "C00", "D00", "E00", "F00", "H00", "I00", "J00", "K00", "L00", "M00", "N00", "O00" };
+            int idxKodeBuah = (int)buah;
+            return kodeBuah[idxKodeBuah];
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KPL_1302201138_MOD3_JURNAL_GGR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
